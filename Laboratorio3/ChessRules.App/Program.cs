7357c1368942ac7dcd8389675260bc9e3ec6d39c
Andrea Aguirre <56442641+andreasmm16@users.noreply.github.com>
2023-06-26 20:44:30 -0600

    Add files via upload

---
+++ Laboratorio3/ChessRules.App/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using ChessRules.Infrastructure.Serializers;
+using ChessRules.Infrastructure.Readers;
+using ChessRules.Core;
+using ChessRules.Infrastructure.Loggers;
+
+internal class Program
+{
+    //Design patterns used:
+    //1. Rules Engine Pattern
+    //2. Builder Pattern
+    private static void Main(string[] args)
+    {
+        var chessGame = new ChessGame(new ConsoleLogger(), new JsonFileReader(), new JsonMovementsDeserializer(), new ChessBoard());
+        chessGame.validateMovements();
+    }
+}
